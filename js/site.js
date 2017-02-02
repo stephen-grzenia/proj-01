4c52f2cfcce111b04e8f5d6d685a3e07150faaef
stephen-grzenia <pikathechu7@gmail.com>
2017-02-02 13:57:57 -0600

    updated all files(using prof stolleys demo code) to be able to hide the submit button until a certain requirement is filled out. I want to add more of my own, this is to help me understand how itll interact with my site

--- js/site.js
+++ js/site.js
@@ -1,1 +1,20 @@
-********placeholder for file
+// Tell jQuery to give up the dollar sign
+$.noConflict();
+
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+
+  $('#email').on('keyup focus blur', function() {
+    var currentValue = $(this).val();
+    var validPattern = $(this).attr('pattern');
+    console.log(currentValue,validPattern);
+    if(currentValue.match(validPattern)) {
+      $('#submit').addClass('active');
+    } else {
+      $('#submit').removeClass('active');
+    }
+  });
+
+});
